a83f98ee2cece47db461de59be1d06244f5f8f89
linux_china <libing.chen@gmail.com>
2019-04-12 14:01:50 -0700

    add stream

--- src/main/kotlin/org/mvnsearch/coroutines/grpc/AccountServiceCoroutineImpl.kt
+++ src/main/kotlin/org/mvnsearch/coroutines/grpc/AccountServiceCoroutineImpl.kt
@@ -1,10 +1,14 @@
 package org.mvnsearch.coroutines.grpc
 
 import com.google.protobuf.Int32Value
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
 import org.lognet.springboot.grpc.GRpcService
 import org.mvnsearch.service.AccountResponse
 import org.mvnsearch.service.AccountServiceImplBase
 import org.mvnsearch.service.GetAccountRequest
+import org.mvnsearch.service.QueryAccountRequest
 
 /**
  * Account Service coroutines implementation
@@ -27,4 +31,21 @@ class AccountServiceCoroutineImpl : AccountServiceImplBase() {
             .setNick("Jackie")
             .build()
     }
+
+    @ExperimentalCoroutinesApi
+    override suspend fun findAll(request: QueryAccountRequest): ReceiveChannel<AccountResponse> =
+        produce<AccountResponse> {
+            send(
+                AccountResponse.newBuilder()
+                    .setId(1)
+                    .setNick("Jackie")
+                    .build()
+            )
+            send(
+                AccountResponse.newBuilder()
+                    .setId(2)
+                    .setNick("James")
+                    .build()
+            )
+        }
 }
